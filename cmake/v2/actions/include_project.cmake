8240a7e05041fd90429f6c5b1d4db6805070cf25
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-07-10 16:25:24 +0500

    fix: Fixed `portable_target_include_project`: added `PORTABLE_TARGET__CURRENT_PROJECT_DIR` variable.

--- cmake/v2/actions/include_project.cmake
+++ cmake/v2/actions/include_project.cmake
@@ -17,6 +17,9 @@ function (portable_target_include_project PATH)
     set(_saved_CMAKE_PROJECT_NAME ${CMAKE_PROJECT_NAME})
     set(_saved_PROJECT_NAME ${PROJECT_NAME})
 
+    get_filename_component(PORTABLE_TARGET__CURRENT_PROJECT_DIR
+        ${PATH} DIRECTORY)
+
     include(${PATH})
 
     set(CMAKE_PROJECT_NAME ${_saved_CMAKE_PROJECT_NAME})
